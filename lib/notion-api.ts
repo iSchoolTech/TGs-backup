15807023f15cf345afb0cff7b956c29cef6c8f97
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2024-04-14 10:41:56 +0200

    updating beta

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,5 +1,7 @@
 import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
+  activeUser: process.env.NOTION_ACTIVE_USER,
+  authToken: process.env.NOTION_TOKEN_V2,
   apiBaseUrl: process.env.NOTION_API_BASE_URL
 })
